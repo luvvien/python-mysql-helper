4b755b2c284828eedf7900d9a2d11704559cdb93
Vien <xiaowei@magicmoma.com>
2019-02-28 22:47:41 +0800

    first release

---
+++ README.md
@@ -0,0 +1,153 @@
+- Program : MySQL db helper
+- Author : Vien
+- History
+  - first release, support py2&py3  2017/09/05  Vien
+
+## Class
+**Constructor**
+```
+def __init__(self, host, user, passwd, schema, charset='utf8mb4',
+                 autocommit=True, mincached=1, maxcached=5, maxshared=5)
+```
+sample 1 :
+
+```
+db_conf = {
+            'user' : 'root',
+            'passwd' : 'moma',
+            'host' : '127.0.0.1',
+            'schema' : 'dbname',
+            'charset' : 'utf8mb4',
+            'mincached' : 1,
+            'maxcached' : 3,
+            'maxshared' : 3,
+        }
+with DBHelper(**db_conf) as db:
+    db.execute(" select 1 ") 
+```
+
+sample 2:
+
+```
+db = DBHelper('127.0.0.1', 'root', 'moma', 'dbname')
+db.execute(" select 1 ")
+db.close()
+```
+
+> If u not use `with ... as ...` , u need to call close().
+
+
+
+## Function
+
+**get_last_sql(self)**
+
+- 获取当前cursor执行的最后一条sql
+
+**get_rowcount(self)**
+
+- 获取当前cursor执行的最后一条sql的影响行数
+
+**begin(self) & end(self)**
+
+- 管理事务（默认情况下是自动管理事务）
+
+  ```
+  with DBHelper(**db_conf) as db:
+      db.begin() # 开启事务
+      db.execute(sql)
+      db.execute_batch(sql)
+      db.insert_batch(sql)
+      db.end() # 关闭事务，这里一定要记得关闭操作，关闭操作包括提交事务和还原成自动管理事务
+  ```
+
+**use_schema(self, schema)**
+
+- 切换数据库
+- 其中schema是数据库名称
+
+**get_last_id(self, table, pri='id')**
+
+- 获取table最后一个主键的值
+- 其中pri是主键名称，默认为id
+
+**show_variable(self, var_name)**
+
+- 获取数据库参数配置
+
+- var_name是变量名，例如
+
+  ```
+  show_variable('max_allowed_packet') # 获取传送数据包最大值
+  ```
+
+**get_dicts(self, sql, params=None, convert=True)**
+
+- 返回一个字典型（key-value，也就是字段名和值的组合）的list
+- params是参数，可以是单个值，也可以是tuple或者list
+- convert是是否转为list，默认为list，设置False会以tuple形式返回
+
+**get_values(self, sql, params=None, convert=True)**
+
+- 返回一个结果集list（不包括字段名称）
+- params是参数，可以是单个值，也可以是tuple或者list
+- convert是是否转为list，默认为list，设置False会以tuple形式返回
+
+**execute(self, sql, params=None)**
+
+- 执行一条sql
+- params是参数，可以是单个值，也可以是tuple或者list
+
+**execute_batch(self, sql, params, step_size=2000)**
+
+- 批量执行sql
+- params是参数，可以是tuple或者list
+- step_size是一次执行数量，默认2000
+
+**insert_batch(self, sql, params, step_size=2000)**
+
+- 批量插入
+- params是参数，可以是tuple或者list
+- step_size是一次执行数量，默认2000，如果一条记录很大，请不要将此参数设置过大，超过max_allowed_packet的大小可能会出问题
+
+**create_table_like(self, target, origin)**
+
+- 复制表（仅复制结构）
+- target是新表的名字，origin是所要复制的表的名字
+
+**load_data_infile(self, path, table, ignore_row=0, local=False, fields_terminated=',', enclosed='"',
+                         lines_terminated='\\n', ignore=True
+                         )**
+
+- 从文件导入数据到表中
+- 需要传入文件路径path和表名table，这两项是必须的
+- 其他参数可选，分别是忽略的行数ignore_row，默认0，对有标头的表格可以忽略一行
+- local参数是选择文件导入的位置是从mysql服务所在的主机还是从本机，默认False是与mysql服务同一主机的文件
+- fields_terminated，enclosed，lines_terminated是几种分割换行字符，提供默认值
+- ignore是选择是否插入重复数据，默认忽略重复数据
+
+**select_data_outfile(self, sql, path, fields_terminated=',', enclosed='"',
+                            lines_terminated='\\n')**
+
+- 从表查找数据导出到文件
+- 其中sql和path是必须的参数，分别是查询sql以及文件存放路径，如果已存在同名文件会失败。
+- 后面三项是分割换行字符，提供默认值
+
+**get_fields(self, table, no_pri=False)**
+
+- 获取table的字段名
+- no_pri是是否包含主键信息，默认返回，设置True为忽略主键
+
+**get_fields_and_types(self, table, no_pri=False)**
+
+- 获取table的字段名和类型
+- no_pri是是否包含主键信息，默认返回，设置True为忽略主键
+
+**get_fields_types_lens(self, table, no_pri=False)**
+
+- 获取table的字段名、类型和长度
+- no_pri是是否包含主键信息，默认返回，设置True为忽略主键
+
+**show_create_table(self, table)**
+
+- 获取table的建表语句
---
+++ vndb.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+try:
+    import MySQLdb as DB
+    xrg = xrange
+except:
+    import pymysql as DB
+    xrg = range
+
+" MySQL OP Helper "
+
+__author__ = 'Vien'
+
+
+class DBHelper(object):
+    def __init__(self, host, user, passwd, schema, charset='utf8mb4',
+                 autocommit=True):
+        self._db_conf = {
+            'user': user,
+            'passwd': passwd,
+            'host': host,
+            'db': schema,
+            'charset': charset
+        }
+        self._autocommit = autocommit
+        self._conn = None
+        self._dict_cursor = None
+        self._tuple_cursor = None
+        self._current_cursor = None
+
+    def __enter__(self):
+        self.get_conn()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    def escape_str(self, s):
+        return DB.escape_string(s)
+
+    def literal(self, s):
+        return self._conn.literal(s)
+
+    def format_param(self, param, batch=False):
+        if param:
+            if batch:
+                return [i if (isinstance(i, tuple) or isinstance(i, list)) else [i] for i in param]
+            return ((isinstance(param, tuple) or isinstance(param, list)) and [param] or [[param]])[0]
+        return None
+
+    def get_last_sql(self):
+        if self._current_cursor:
+            return self._current_cursor._executed
+        return False
+
+    def get_rowcount(self):
+        if self._current_cursor:
+            return int(self._current_cursor.rowcount)
+        return False
+
+    def auto_commit(self, flag=True):
+        self._autocommit = flag
+
+    def begin(self):
+        self._autocommit = False
+
+    def end(self):
+        self._conn.commit()
+        self._autocommit = True
+
+    def use_schema(self, schema):
+        try:
+            self._conn.select_db(schema)
+        except Exception as e:
+            raise e
+
+    def get_conn(self):
+        if self._conn:
+            self._conn.ping()
+            return self._conn
+        try:
+            self._conn = DB.connect(**self._db_conf)
+        except Exception as e:
+            raise e
+        return self._conn
+
+    def get_cursor(self, cursorclz=None):
+        if not self._conn:
+            self.get_conn()
+        else:
+            self._conn.ping()
+        try:
+            cursor = self._conn.cursor(cursorclz)
+        except Exception as e:
+            raise e
+        return cursor
+
+    def close(self):
+        if self._conn:
+            try:
+                if self._dict_cursor:
+                    self._dict_cursor.close()
+                if self._tuple_cursor:
+                    self._tuple_cursor.close()
+                if self._conn:
+                    self._conn.close()
+            except Exception as e:
+                raise e
+
+    def get_last_id(self, table, pri='id'):
+        last_id = self.get_values(" SELECT `%s` FROM `%s` ORDER BY `%s` DESC LIMIT 1 " % (pri, table, pri))
+        return last_id[0] if last_id else False
+
+    def show_variable(self, var_name):
+        res = self.get_values(" SHOW VARIABLES LIKE '%{}%' ".format(var_name))
+        return res[0] if res else False
+
+    def get_dicts(self, sql, params=None, convert=True):
+        if not self._dict_cursor:
+            self._dict_cursor = self.get_cursor(DB.cursors.DictCursor)
+        else:
+            self._conn.ping()
+        try:
+            self._dict_cursor.execute(sql, self.format_param(params))
+            self._current_cursor = self._dict_cursor
+            res = self._dict_cursor.fetchall()
+            if res:
+                if convert:
+                    return list(res)
+                else:
+                    return res
+        except Exception as e:
+            raise e
+        return False
+
+    def get_values(self, sql, params=None, convert=True):
+        if not self._tuple_cursor:
+            self._tuple_cursor = self.get_cursor()
+        else:
+            self._conn.ping()
+        try:
+            self._tuple_cursor.execute(sql, self.format_param(params))
+            self._current_cursor = self._tuple_cursor
+            res = self._tuple_cursor.fetchall()
+            if res:
+                if convert:
+                    res_list = [(len(item) == 1 and [item[0]] or [item])[0] for item in res]
+                    return res_list
+                else:
+                    return res
+        except Exception as e:
+            raise e
+        return False
+
+    def execute(self, sql, params=None):
+        if not self._tuple_cursor:
+            self._tuple_cursor = self.get_cursor()
+        else:
+            self._conn.ping()
+        try:
+            self._tuple_cursor.execute(sql, self.format_param(params))
+            self._current_cursor = self._tuple_cursor
+            if self._autocommit:
+                self._conn.commit()
+        except Exception as e:
+            self._conn.rollback()
+            raise e
+        return self.get_rowcount()
+
+    def execute_batch(self, sql, params, step_size=2000):
+        if not params:
+            raise Exception("No params")
+        if not self._current_cursor:
+            self._tuple_cursor = self.get_cursor()
+            self._current_cursor = self._tuple_cursor
+        else:
+            self._conn.ping()
+        row_count = 0
+        try:
+            size = int(step_size)
+            length = len(params)
+            size = size if length >= size else length
+            params = self.format_param(params, True)
+            for i in xrg(0, (length + size - 1) // size):
+                self._current_cursor.executemany(sql, params[i * size:(i + 1) * size])
+                row_count = row_count + self.get_rowcount()
+                if self._autocommit:
+                    self._conn.commit()
+        except Exception as e:
+            self._conn.rollback()
+            raise e
+        return row_count
+
+    def insert_batch(self, sql, params, step_size=2000):
+        if not params:
+            raise Exception("No params")
+        if not self._current_cursor:
+            self._tuple_cursor = self.get_cursor()
+            self._current_cursor = self._tuple_cursor
+        else:
+            self._conn.ping()
+        try:
+            size = int(step_size)
+            length = len(params)
+            size = size if length >= size else length
+            params = self.format_param(params, True)
+            pos = sql.rfind('(')
+            values = []
+            for i in xrg(0, (length + size - 1) // size):
+                sql_left = sql[:pos]
+                sql_right = sql[pos:]
+                for args in params[i * size:(i + 1) * size]:
+                    for arg in args:
+                        values.append(arg)
+                    sql_left = sql_left + sql_right + ','
+                sql_final = sql_left[:sql_left.rfind(',')]
+                # print(sql_final)
+                self._current_cursor.execute(sql_final, values)
+                if self._autocommit:
+                    self._conn.commit()
+        except Exception as e:
+            self._conn.rollback()
+            raise e
+        return self.get_rowcount()
+
+    def get_fields_and_types(self, table, no_pri=False):
+        sql = ' DESC %s ' % table
+        res = self.get_dicts(sql)
+        props = dict()
+        for item in res:
+            if no_pri and item['Key'] == 'PRI':
+                continue
+            type = item['Type']
+            pos = type.find('(')
+            type = type[:pos if pos != -1 else len(type)]  # pos != -1 and pos or len(type)
+            props[item['Field']] = type
+        return props
+
+    def get_fields_types_lens(self, table, no_pri=False):
+        sql = ' DESC %s ' % table
+        res = self.get_dicts(sql)
+        props = dict()
+        for item in res:
+            if no_pri and item['Key'] == 'PRI':
+                continue
+            props[item['Field']] = item['Type']
+        return props
+
+    def get_fields(self, table, no_pri=False):
+        props = self.get_fields_types_lens(table, no_pri)
+        return [k for k, v in props.items()]
+
+    def show_create_table(self, table):
+        sql = ' SHOW CREATE TABLE %s ' % table
+        res = self.get_dicts(sql)
+        return res[0]['Create Table'] if res else False
+
+    def create_table_like(self, target, origin):
+        return self.execute("CREATE TABLE IF NOT EXISTS `{}` LIKE `{}`".format(target, origin))
+
+    def load_data_infile(self, path, table, ignore_row=0, charset='utf8mb4', local=False, fields_terminated=',', enclosed='"',
+                         lines_terminated='\\n', ignore=True
+                         ):
+        sql = "LOAD{}DATA INFILE '{}'{}INTO TABLE `{}` CHARACTER SET {} " \
+              "FIELDS TERMINATED BY '{}' ENCLOSED BY '{}' LINES TERMINATED BY '{}' " \
+              "IGNORE {} LINES ".format(' LOCAL ' if local else ' ', path, ' IGNORE ' if ignore else ' ', table,
+                                        fields_terminated, charset, enclosed, lines_terminated, ignore_row)
+        return self._current_cursor.execute(sql)
+
+    def select_data_outfile(self, sql, path, charset='utf8mb4', fields_terminated=',', enclosed='"',
+                            lines_terminated='\\n'):
+        sql = sql + " INTO OUTFILE '{}' CHARACTER SET {} FIELDS TERMINATED BY '{}' ENCLOSED BY '{}' " \
+                    "LINES TERMINATED BY '{}' ".format(path, charset, fields_terminated, enclosed, lines_terminated)
+        return self._current_cursor.execute(sql)
+
+
+if __name__ == '__main__':
+    db_conf = {
+        'user': 'vien',
+        'passwd': 'vien',
+        'host': '127.0.0.1',
+        'schema': 'vien',
+        'charset': 'utf8mb4'
+    }
+    with DBHelper(**db_conf) as db:
+        print(db.show_variable("max_allowed_packet"))
